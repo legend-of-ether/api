c0d6ef0b280b65fc68ae8a6c69b027b9cb0060b0
Lautaro Dragan <lautaro.dragan@gmail.com>
2018-05-26 14:31:34 -0300

    notify sign off

--- src/index.js
+++ src/index.js
@@ -8,28 +8,67 @@ const sockets = []
 io.on('connection', function(socket){
   console.log('user connected')
 
+  socket.on('disconnect', () => {
+    const playerSocket = sockets.find(_ => _.socket === socket)
+
+    if (!playerSocket)
+      return
+
+    const player = players.find(_ => _.id === playerSocket.playerId)
+    const stringifiedPlayer = JSON.stringify(player)
+
+    console.log('user disconnected', player.id)
+
+    sockets.splice(sockets.indexOf(playerSocket), 1)
+    player.connected = false
+
+    const emitUserSignedOff = socket => socket.emit(
+      'userSignedOff',
+      stringifiedPlayer,
+    )
+
+    sockets
+      .map(hydratePlayerSocket)
+      .filter(_ => _.player.connected)
+      .map(_ => _.socket)
+      .forEach(emitUserSignedOff)
+  })
+
   socket.on('signIn', id => {
     console.log('signIn', id)
 
-    const player = players.find(player => player.id === id)
+    const signIn = id => {
+      const existingPlayer = players.find(player => player.id === id)
 
-    if (!player) {
-      const newPlayer = {
-        id,
-        x: 0,
-        y: 0,
+      if (existingPlayer) {
+        existingPlayer.connected = true
+        return existingPlayer
+      } else {
+        const newPlayer = {
+          id,
+          x: 0,
+          y: 0,
+          connected: true,
+        }
+        players.push(newPlayer)
+        return newPlayer
       }
-      players.push(newPlayer)
     }
+
+    const player = signIn(id)
+    const stringifiedPlayer = JSON.stringify(player)
 
     socket.emit('signInSuccess', JSON.stringify(players))
 
-    sockets.forEach(
-      socket => socket.socket.emit(
-        'userSignedIn',
-        JSON.stringify(players.find(_ => _.id === id))
-      )
+    const emitUserSignedIn = socket => socket.emit(
+      'userSignedIn',
+      stringifiedPlayer,
     )
+
+    sockets
+      .filter(isConnected)
+      .map(_ => _.socket)
+      .forEach(emitUserSignedIn)
 
     sockets.push({
       playerId: id,
@@ -71,3 +110,12 @@ const minMax = (min, max, val) => Math.min(Math.max(val, min), max)
 const directionToNumberX = arrow => arrow === 'Right' ? 1 : arrow === 'Left' ? -1 : 0
 
 const directionToNumberY = arrow => arrow === 'Down' ? 1 : arrow === 'Up' ? -1 : 0
+
+const playerById = (id) => players.find(_ => _.id === id)
+
+const hydratePlayerSocket = ({socket, playerId}) => ({
+  player: playerById(playerId),
+  socket,
+})
+
+const isConnected = player => player.connected
