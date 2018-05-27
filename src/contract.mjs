d916f04155b43eb868472d81bbb3bc68fdf39fae
lautarodragan <lautaro.dragan@gmail.com>
2018-05-27 06:48:42 -0300

    fix

--- src/contract.mjs
+++ src/contract.mjs
@@ -24,33 +24,15 @@ export async function getItems() {
 
 export async function grabItem(who, what) {
   console.log('grabItem', who, what)
-
-  // const testFn = TestContract.methods.test(testAddress)
-  // const gas = await testFn.estimateGas()
-  // const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
-  // const data = testFn.encodeABI()
-  // const nonce = await web3.eth.getTransactionCount(SENDER_ADDRESS, 'pending')
-  // const payload = {
-  //   nonce,
-  //   data,
-  //   gas,
-  //   from: SENDER_ADDRESS,
-  //   to: testContractAddress
-  // }
-  // const signedTx = await account.signTransaction(payload, account.privateKey)
-  // const { rawTransaction } = signedTx
-  // const response = await web3.eth.sendSignedTransaction(rawTransaction)
+  console.log('contract address', contract.networks['3'].address)
 
   const PRIVATE_KEY = '0xf0db0a4c7668cefdfce4e53522ecbebdbe6fbc21463d4da4c2a4351004e0f3d4'
   const SENDER_ADDRESS = '0x209f210ec4dd7beae894a45666faadf0b6be0dbf'
-  const method = contractInstance.methods.transferItemOwnership(0, who, what)
-  const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
+  const method = contractInstance.methods.addItem(who, what)
   const gas = await method.estimateGas()
-  console.log('gas', gas)
+  const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
   const data = method.encodeABI()
-  console.log('data', data)
   const nonce = await web3.eth.getTransactionCount(SENDER_ADDRESS, 'pending')
-  console.log('nonce', nonce)
   const payload = {
     nonce,
     data,
@@ -58,15 +40,13 @@ export async function grabItem(who, what) {
     from: who,
     to: contract.networks['3'].address
   }
-  console.log('grabItem signTransaction', account)
   const signedTx = await account.signTransaction(payload)
-  console.log('grabItem signedTx', signedTx)
   const { rawTransaction } = signedTx
-  console.log('grabItem rawTransaction', rawTransaction)
   const response = await web3.eth.sendSignedTransaction(rawTransaction)
 
   console.log('grabItem response', response)
+}
 
-  // const result = await contractInstance.methods.transferItemOwnership(0, who, what).send()
-  // console.log('grab item result', result)
+async function createItem() {
+  console.log('createItem', who, what)
 }
